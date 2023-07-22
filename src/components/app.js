b4c0050d2c445bf2c8cb3a43ffc2c9aa7d96044e
Hantzo Studios <haon.00@gmail.com~>
2023-07-22 14:22:48 +0200

    version segunda

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import moment from 'moment/moment';
+  import React, { Component } from 'react';
+  import moment from 'moment/moment';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
-        <h1>HantzoStudios</h1>
-        <h2>Estamos de vuelta!</h2>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')} 
-      </div>
-      </div>
-     
+  // Tenemos que imporetar el componente que acabamos de crear, como es nuestra creacion, debemeos de ponerle la ruta
+  import PortfolioContainer from "./portfolio/portfolio-container";
 
-    );
+  // esto no entiendo muy bien
+  export default class App extends Component {
+    render() {
+      return (
+        <div className='app'>
+          <h1>HantzoStudios</h1>
+          <h2>Estamos de vuelta!</h2>
+          
+          
+            <div>
+              {moment().format('MMMM Do YYYY, h:mm:ss a')} 
+            </div>
+
+            {/* y ahora ya podemos meter el componente en el render */}
+            <PortfolioContainer />
+        </div>
+      
+
+      );
+    }
   }
-}
